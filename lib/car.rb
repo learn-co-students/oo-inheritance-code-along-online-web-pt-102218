c540f3de30c0df484496e52e43032313aa557a95
Johnathon Wood <jaywood128@gmail.com>
2018-11-28 19:35:06 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,25 @@
 require_relative "./vehicle.rb"
+
+class  Car < Vehicle
+
+#   attr_accessor :wheel_size, :wheel_number
+# @@all = []
+#   def initialize(wheel_size, wheel_number)
+#     @wheel_size = wheel_size
+#     @wheel_number = wheel_number
+#     @@all << self
+#   end
+#
+#   def wheel_size
+#     @wheel_size
+#   end
+#
+#   def wheel_number
+#     @wheel_number
+#   end
+
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+
+end
