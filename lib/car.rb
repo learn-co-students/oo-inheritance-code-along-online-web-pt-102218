2fe068cf1dd998e88bee7f915d2439b131c50db8
tatiana.mojica <tatiana.mojica@bustle.com>
2018-12-09 22:56:50 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,14 @@
 require_relative "./vehicle.rb"
+#subclasses 
+# needs access to the Vehicle class and will therefore need access to the file that contains class 
+
+class Car < Vehicle
+  #< inherits the Car class FROM vehicle
+  # inherits the Vehicle methods and therefor have access to them
+  #we are now overwriting the inherited go method with one specific to Car 
+  
+  def go 
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end 
+  
+end 
